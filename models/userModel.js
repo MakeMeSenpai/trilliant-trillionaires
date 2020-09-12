23eb1e0f4bd2b07ea3a3e13314cb03bde0b7edfa
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-09-12 13:54:19 -0700

    test

---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+// js object on how users store their data in our database
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true, minlength: 5 },
+    displayName: { type: String },
+})
+
+// how we interact with any entries from user
+module.exports = User = mongoose.model("user", userSchema);
