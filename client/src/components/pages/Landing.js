8b1ce36948ef0d00074748e51c451f2e43b4fe54
shaannessy25 <shaan.hurley25@gmail.com>
2020-10-15 17:35:57 -0700

    adding footer

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Button, Grid, Container, Box } from '@material-ui/core';
+import { Button, Grid, Box } from '@material-ui/core';
+import { Container, Row, Col } from 'react-bootstrap';
+
 import Register from '../auth/Register'
 import "./Landing.css"
 
@@ -9,12 +11,48 @@ export class Landing extends Component {
         return (
             <div className="landing-main">
                 <div className="section-header">
-                    <Grid>
-                        <Register />
-                    </Grid>
+                    <Container>
+                        <Row>
+
+                            <Col>
+                                <div className="section-statement">
+                                    <h1>Trilliant</h1>
+                                    <p>Trilliant is a product review company that.</p>
+                                </div>
+                            </Col>
+                            <Col>
+                                <Register />
+                            </Col>
+                        </Row>
+                    </Container>
+                    
                 </div>
                 <div className="section-about">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/QBMuN3VkjbQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <div>
+                    <div className="page-footer">
+                        <ul>
+                        <li><h3>Column 1</h3></li>
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                        <li><a>Item 3</a></li>
+                        </ul>
+
+                        <ul>
+                        <li><h3>Column 2</h3></li>
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                        <li><a>Item 3</a></li>
+                        </ul>
+
+                        <ul>
+                        <li><h3>Column 3</h3></li>
+                        <li><a>Item 1</a></li>
+                        <li><a>Item 2</a></li>
+                        <li><a>Item 3</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
