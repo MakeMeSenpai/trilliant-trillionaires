4e8f3e0cf23b8ef92401be6ae67dc76a649c96de
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-11-16 19:03:04 -0800

    added search icon

--- client/src/components/products/Product.js
+++ client/src/components/products/Product.js
@@ -4,6 +4,9 @@ import product from '../../trilliant.json'
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import './product.css'
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Popover from '@material-ui/core/Popover';
+import SearchIcon from '@material-ui/icons/Search';
 
 function searchingFor(term) {
     return function (x) {
@@ -30,12 +33,14 @@ export default class Products extends Component {
             <div className="product-display">
 
                 <Autocomplete
+
                     className="searchTerm"
                     freeSolo
                     id="free-solo-2-demo"
                     disableClearable
                     options={this.props.products.map((product) => product.name)}
                     renderInput={(params) => (
+
                         <TextField
                             {...params}
                             label="Search input"
@@ -43,18 +48,23 @@ export default class Products extends Component {
                             variant="outlined"
                             onChange={this.searchHandler}
                             value={term}
-                            InputProps={{ ...params.InputProps, type: 'search' }}
+                            InputProps={{
+                                ...params.InputProps, type: 'search',
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <SearchIcon fontSize="large" />
+                                    </InputAdornment>
+                                )
+                            }}
+                            placeholder="Search clothes..."
+
 
                         />
+
                     )}
                 />
-
-
-
-
                 <div className="products" >
                     {this.props.products.filter(searchingFor(term)).map((product) => (
-
                         <div className="product" key={product._id}>
                             <a href={product.url}>
                                 <img src={product.image} alt={product.name}></img>
@@ -69,18 +79,8 @@ export default class Products extends Component {
                         </div>
 
                     ))}
+
                 </div>
-
-
-
-
-
-
-
-
-
-
-
             </div>
         );
     }
