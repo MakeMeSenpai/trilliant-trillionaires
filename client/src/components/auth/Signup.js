56a09bf0c71677b2d9767dd13f646f642b82ad0b
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-12-07 19:13:50 -0800

    updated signup

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -8,7 +8,6 @@ import './Form.css'
 
 export default function Signup() {
     const emailRef = useRef()
-    const userRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const displayNameRef = useRef()
@@ -29,65 +28,70 @@ export default function Signup() {
         try {
             setError('')
             setLoading(true)
-            await signup(emailRef.current.value, userRef.current.value, passwordRef.current.value)
-            history.push("/catelog")
+            await signup(emailRef.current.value, passwordRef.current.value)
+            history.push("/catalog")
 
         } catch {
             setError('Failed to create an account')
         }
 
-
         setLoading(false)
     }
 
+    var sectionStyle = {
+        width: "100",
+        height: "100",
+        backgroundImage: "url(" + Background + ")"
+    };
 
 
     return (
 
+        <section style={sectionStyle}
+        >
+
+            <div className="align">
+
+                <form onSubmit={handleSubmit} className="form" >
+
+                    {error}
+
+                    <label htmlFor="register-email">Email</label>
+
+                    <input
+                        id="register-email"
+                        type="email"
+                        ref={emailRef}
+                        placeholder="Enter Email Address"
+
+                    />
+
+                    <label htmlFor="register-password">Password</label>
+                    <input
+                        id="register-password"
+                        type="password"
+                        ref={passwordRef}
+                        placeholder="Enter Password"
+
+                    />
+                    <label htmlFor="register-password">Verify Password</label>
+
+                    <input
+                        type="password"
+                        placeholder="Verify password"
+                        ref={passwordConfirmRef}
+
+                    />
 
 
 
-        <div className="align">
-
-            <form onSubmit={handleSubmit} className="form" >
-
-                {error}
-
-                <label htmlFor="register-email">Email</label>
-                <input
-                    id="register-email"
-                    type="email"
-                    ref={emailRef}
-                    placeholder="Enter Email Address"
-
-                />
-
-                <label htmlFor="register-password">Password</label>
-                <input
-                    id="register-password"
-                    type="password"
-                    ref={passwordRef}
-                    placeholder="Enter Password"
-
-                />
-                <label htmlFor="register-password">Verify Password</label>
-
-                <input
-                    type="password"
-                    placeholder="Verify password"
-                    ref={passwordConfirmRef}
-
-                />
-
-
-
-                <button disabled={loading} type="submit" className="btn-login">Sign Up</button>
-                <div className="w-100 text-center mt-2">
-                    Already have an account? <Link to="/login">Log in</Link>
-                </div>
-            </form>
-        </div>
+                    <button disabled={loading} type="submit" className="btn-login">Sign Up</button>
+                    <div className="w-100 text-center mt-2">
+                        Already have an account? <Link to="/login">Log in</Link>
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 
 }
-
